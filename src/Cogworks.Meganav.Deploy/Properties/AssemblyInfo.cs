8756a3d7323ec5d44ef8243aa5f6579e18d21bcc
Ronald Barendse <ronald@panoramastudios.nl>
2018-06-06 17:36:17 +0200

    Added Cogworks.Meganav.Deploy

---
+++ src/Cogworks.Meganav.Deploy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cogworks.Meganav.Deploy")]
+[assembly: AssemblyDescription("Umbraco Deploy support for Cogworks.Meganav.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cogworks")]
+[assembly: AssemblyProduct("Cogworks Meganav")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("870bf029-afb8-42a8-bb81-cbc5f9a45f28")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
